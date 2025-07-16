b5082b668c8b5ae38b6a082aae64403056d515f5
chandrakesh <chandrakeshram31@gmail.com>
2025-07-16 18:56:39 +0530

    Initial Spring Boot Project

---
+++ src/main/java/com/cansupport/CansupportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cansupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CansupportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CansupportBackendApplication.class, args);
+	}
+
+}
