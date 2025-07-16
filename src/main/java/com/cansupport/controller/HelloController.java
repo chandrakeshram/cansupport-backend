1e73ac39a8d8cdf1dca35008a1ac479e52522a69
Chandrakesh <chandrakeshram31@gmail.com>
2025-07-16 21:02:02 +0530

    Added Controller to check whether backend and database connectivity is there

---
+++ src/main/java/com/cansupport/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.cansupport.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloController {
+    @GetMapping("path")
+    public String hello() {
+        return "Hello! CAN Support Backend is Working";
+    }
+
+}
